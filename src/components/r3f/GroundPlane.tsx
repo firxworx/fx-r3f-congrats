5851c3a3c4d3954f4cb9cdadd8a057c7cd1b7c38
Kevin Firko <kfirko@bitcurve.com>
2021-07-31 23:09:37 -0400

    cleanup GroundPlane and remove unused cannon / usePlane

--- src/components/r3f/GroundPlane.tsx
+++ src/components/r3f/GroundPlane.tsx
@@ -1,20 +1,13 @@
 import React from 'react'
-import { usePlane } from '@react-three/cannon'
 
+/**
+ * Plane at origin, rotated 90-degrees along the x-axis, to serve as the ground.
+ */
 export const GroundPlane: React.FC = () => {
-  const [ref] = usePlane(() => ({
-    rotation: [-Math.PI / 2, 0, 0],
-    position: [0, 0, 0],
-    material: {
-      friction: 0.5,
-    },
-  }))
-
   return (
-    <mesh ref={ref} receiveShadow={true} scale={[40, 40, 40]}>
+    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow={true} scale={[40, 40, 40]}>
       <planeBufferGeometry />
       <meshPhongMaterial color={'#8BD8F8'} transparent opacity={0.5} />
-      {/* <meshStandardMaterial map={texture} attach="material" /> */}
     </mesh>
   )
 }
